3420b6607e997b636bea062389a3d13bac34cfcb
SerVErThread E <wedgg@yahoo.com>
2023-10-03 19:19:56 +0000

    #23 bucket should no longer userandom provider

--- main.tf
+++ main.tf
@@ -1,19 +1,11 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "website_bucket" {
   # Bucket naming rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = random_string.bucket_name.result
+  bucket = var.bucket_name
 
   
   tags = {
     UserUuid = var.user_uuid
   }
-}
-resource "random_string" "bucket_name" {
-  lower           = true
-  length           = 32
-  special          = false
-  upper = false
-  min_numeric = 3
-
 }
--- variables.tf
+++ variables.tf
@@ -8,3 +8,16 @@ variable "user_uuid" {
         error_message = "The user_uuid must be in UUID format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx where 'x' is a hexadecimal character."
     }
 }
+variable "bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket"
+
+  validation {
+    condition     = (
+                    length(var.bucket_name) > 2 && 
+                    length(var.bucket_name) < 64 && 
+                    regex("^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$", var.bucket_name) != null
+                    )
+    error_message = "Bucket name must be between 3 and 63 characters, and follow AWS bucket naming rules: start and end with a lowercase letter or number, and contain only lowercase letters, numbers, hyphens, and dots."
+  }
+}
