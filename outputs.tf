361595b9906a947c0721a0857d740440a40f84fb
SerVErThread E <wedgg@yahoo.com>
2023-10-04 12:10:28 +0000

    #27 enable staic website hosting.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
 }
+output "S3_website_endpoint" {
+  description = "S3 static website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+  #aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
