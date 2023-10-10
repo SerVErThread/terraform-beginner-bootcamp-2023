1827bf839ef3a1d3f8a6fe020883405755170fe3
SerVErThread E <wedgg@yahoo.com>
2023-10-10 13:38:51 +0000

    Rollback

--- variables.tf
+++ variables.tf
@@ -5,11 +5,11 @@ variable "bucket_name" {
   type        = string
 }
 
-variable "index_html_filepath" {
-  type        = string
-}
+# variable "index_html_filepath" {
+#   type        = string
+# }
 
 
-variable "error_html_filepath" {
-  type        = string
-}
+# variable "error_html_filepath" {
+#   type        = string
+# }
