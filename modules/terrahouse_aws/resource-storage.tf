195d983480655b0c64a60146edbfda0d37c70225
SerVErThread E <wedgg@yahoo.com>
2023-10-10 13:41:22 +0000

    Test

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -34,28 +34,28 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 #     content_type = "text/html"
 #   etag = filemd5("${path.root}${var.error_html_filepath}")
 #  }
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-  policy = jsondecode({
-    "Version" = "2012-10-17",
-    "Statement" = {
-        "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
-        "Effect"= "Allow",
-        "Principal"= {
-            "Service"= "cloudfront.amazonaws.com"
-            },
-        "Action" = "s3:GetObject",
-        "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}",
-        "Condition" = {
-        "StringEquals" = {
-            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
-           # "AWS:SourceArn": data.aws_caller_identity.current.arn
-         }
-       }
-    }
-  })
-}
+# resource "aws_s3_bucket_policy" "bucket_policy" {
+#   bucket = aws_s3_bucket.website_bucket.bucket
+#   #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+#   policy = jsondecode({
+#     "Version" = "2012-10-17",
+#     "Statement" = {
+#         "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
+#         "Effect"= "Allow",
+#         "Principal"= {
+#             "Service"= "cloudfront.amazonaws.com"
+#             },
+#         "Action" = "s3:GetObject",
+#         "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}",
+#         "Condition" = {
+#         "StringEquals" = {
+#             "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+#            # "AWS:SourceArn": data.aws_caller_identity.current.arn
+#          }
+#        }
+#     }
+#   })
+# }
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.id
 
