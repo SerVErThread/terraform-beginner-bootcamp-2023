8114b116a4ff38a0c66fa85bf8cfd6a86aa003f6
SerVErThread E <wedgg@yahoo.com>
2023-10-16 19:18:17 +0000

    #42 should be able to run a terraform provider block without errors

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
-output "S3_website_endpoint" {
-  description = "S3 static website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-  #aws_s3_bucket_website_configuration.website_configuration.website_endpoint
-}
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
+# output "S3_website_endpoint" {
+#   description = "S3 static website hosting endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+#   #aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "The Cloudfront Distribution Domain Name"
-    value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "The Cloudfront Distribution Domain Name"
+#     value = module.terrahouse_aws.cloudfront_url
+# }
--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "content_version" {
   type        = number
 }
 
-variable "assets_path" {
-  type        = string
-}
+# variable "assets_path" {
+#   type        = string
+# }
   
